d64189cfaebe1cc1c0852a95750fa9232d20aedc
Herkahahaha <herkahahaha@gmail.com>
2019-10-22 00:29:09 +0700

    add react fragment and remove app.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "./App.css";
 import Navbar from "./components/Navbar";
 import Post from "./components/Post";
 import Home from "./components/pages/Home";
@@ -10,15 +9,15 @@ import Contact from "./components/pages/Contact";
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <React.Fragment>
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
-          <Route path="/:post__id" component={Post} />
+          <Route path="/:posts__id" component={Post} />
         </Switch>
-      </div>
+      </React.Fragment>
     </BrowserRouter>
   );
 }
