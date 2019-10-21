9e61da5398811cd2057cda9ccd7d39cdb784eb85
Herkahahaha <herkahahaha@gmail.com>
2019-10-22 00:29:30 +0700

    Add Redux setup

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import "./index.css";
 import "./materialize.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import RootReducer from "./store/reducer/RootReducer";
+const store = createStore(RootReducer);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
